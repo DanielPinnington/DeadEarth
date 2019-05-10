d52b3502932557de024938b2a9e5f3b9a7b131f0
Dan <danielpinnington12@gmail.com>
2019-05-10 14:44:55 +0000

    AI Waypoint && Nav Agents
    AI Waypoints have been added into the scene && the NavMesh Agents will follow incrementally follow the Waypoints, whilst avoiding NavMesh static objects

---
+++ Dead Earth Game/Assets/Navigation Example/NavAgentExample.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentExample : MonoBehaviour
+{
+    public AIWaypointNetwork WaypointNetwork = null;
+    public int CurrentIndex = 0; //Waypoints
+    public bool HasPath = false;
+    public bool PathPending = false;
+    public bool PathStale = false;
+
+    private NavMeshAgent _NavAgent = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _NavAgent = GetComponent<NavMeshAgent>();
+        if (WaypointNetwork == null) return;
+        SetNextDestination(false);
+    }
+
+    void SetNextDestination(bool increment)
+    {
+        // If no network return
+        if (!WaypointNetwork) return;
+
+        // Calculatehow much the current waypoint index needs to be incremented
+        int incStep = increment ? 1 : 0;
+
+        // Calculate index of next waypoint factoring in the increment with wrap-around and fetch waypoint 
+        int nextWaypoint = (CurrentIndex + incStep >= WaypointNetwork.Waypoints.Count) ? 0 : CurrentIndex + incStep;
+        Transform nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
+
+        // Assuming we have a valid waypoint transform
+        if (nextWaypointTransform != null)
+        {
+            // Update the current waypoint index, assign its position as the NavMeshAgents
+            // Destination and then return
+            CurrentIndex = nextWaypoint;
+            _NavAgent.destination = nextWaypointTransform.position;
+            return;
+        }
+
+        // We did not find a valid waypoint in the list for this iteration
+        CurrentIndex++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HasPath = _NavAgent.hasPath;
+        PathPending = _NavAgent.pathPending;
+
+        if(!HasPath && !PathPending)
+        {
+            SetNextDestination(true);
+        }
+
+        if (_NavAgent.isPathStale)
+        {
+            SetNextDestination(false);
+        }
+    }
+}
