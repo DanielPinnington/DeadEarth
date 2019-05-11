e9df81c4e4d00f0ab4b318f5b5f187e057ad8b68
Dan <danielpinnington12@gmail.com>
2019-05-11 15:28:47 +0000

    NavAgent turns to Waypoint and Jumps/Drops
    NavAgent drops and jumps according to the NavMeshOffSet, a graph has been made for the curveture of the jump.

--- Dead Earth Game/Assets/Navigation Example/NavAgentExample.cs
+++ Dead Earth Game/Assets/Navigation Example/NavAgentExample.cs
@@ -13,6 +13,8 @@ public class NavAgentExample : MonoBehaviour
     public bool PathStale = false;
     public NavMeshPathStatus PathStatus = NavMeshPathStatus.PathInvalid;
 
+    public AnimationCurve JumpCurve = new AnimationCurve();
+
     private NavMeshAgent _NavAgent = null;
 
     // Start is called before the first frame update
@@ -61,11 +63,32 @@ public class NavAgentExample : MonoBehaviour
 
         // If we don't have a path and one isn't pending then set the next
         // waypoint as the target, otherwise if path is stale regenerate path
-        if ((!HasPath && !PathPending) || PathStatus == NavMeshPathStatus.PathInvalid /*|| PathStatus==NavMeshPathStatus.PathPartial*/)
+        if (_NavAgent.isOnOffMeshLink)
+        {
+            StartCoroutine(Jump(2.0f));
+        }
+        if ((_NavAgent.remainingDistance <= _NavAgent.stoppingDistance && !PathPending) || PathStatus == NavMeshPathStatus.PathInvalid /*|| PathStatus==NavMeshPathStatus.PathPartial*/)
             SetNextDestination(true);
         else
         if (_NavAgent.isPathStale)
             SetNextDestination(false);
 
+    }
+    IEnumerator Jump (float duration)
+    {
+        OffMeshLinkData data = _NavAgent.currentOffMeshLinkData;
+        Vector3 startPos = _NavAgent.transform.position;
+        Vector3 endPos = data.endPos + (_NavAgent.baseOffset * Vector3.up);
+
+        float time = 0f;
+
+        while(time <= duration)
+        {
+            float t = time / duration;
+            _NavAgent.transform.position = Vector3.Lerp(startPos, endPos, t) + JumpCurve.Evaluate(t) * Vector3.up;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        _NavAgent.CompleteOffMeshLink();
     }
 }
