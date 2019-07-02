affb81c3f3611c8e7744083adfe648f2e174a96f
Dan <danielpinnington12@gmail.com>
2019-07-02 13:53:46 +0000

    Damage System (partially complete)
    Zombie triggers hit (player) with their animations and damage splash screen appears on screen!

--- Dead Earth Game/Assets/Dead Earth/Scripts/AI/AIDamageTrigger.cs
+++ Dead Earth Game/Assets/Dead Earth/Scripts/AI/AIDamageTrigger.cs
@@ -1,43 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class AIDamageTrigger : MonoBehaviour
 {
-    [SerializeField] string _paramater = "";
+    // Inspector Variables
+    [SerializeField] string _parameter = "";
     [SerializeField] int _bloodParticlesBurstAmount = 10;
+    [SerializeField] float _damageAmount = 0.1f;
 
-    //Private
+    // Private Variables
     AIStateMachine _stateMachine = null;
     Animator _animator = null;
     int _parameterHash = -1;
+    GameSceneManager _gameSceneManager = null;
 
-    private void Start()
+    // ------------------------------------------------------------
+    // Name	:	Start
+    // Desc	:	Called on object start-up to initialize the script.
+    // ------------------------------------------------------------
+    void Start()
     {
+        // Cache state machine and animator references
         _stateMachine = transform.root.GetComponentInChildren<AIStateMachine>();
+
         if (_stateMachine != null)
             _animator = _stateMachine.animator;
-            _parameterHash = Animator.StringToHash(_paramater);
+
+        // Generate parameter hash for more efficient parameter lookups from the animator
+        _parameterHash = Animator.StringToHash(_parameter);
+
+        _gameSceneManager = GameSceneManager.instance;
     }
+
+    // -------------------------------------------------------------
+    // Name	:	OnTriggerStay
+    // Desc	:	Called by Unity each fixed update that THIS trigger
+    //			is in contact with another.
+    // -------------------------------------------------------------
     void OnTriggerStay(Collider col)
     {
+        // If we don't have an animator return
         if (!_animator)
             return;
 
+        // If this is the player object and our parameter is set for damage
         if (col.gameObject.CompareTag("Player") && _animator.GetFloat(_parameterHash) > 0.9f)
         {
-            if(GameSceneManager.instance && GameSceneManager.instance.bloodParticles)
+            if (GameSceneManager.instance && GameSceneManager.instance.bloodParticles)
             {
                 ParticleSystem system = GameSceneManager.instance.bloodParticles;
 
-                //Temporary code
+                // Temporary Code
                 system.transform.position = transform.position;
                 system.transform.rotation = Camera.main.transform.rotation;
 
                 system.simulationSpace = ParticleSystemSimulationSpace.World;
                 system.Emit(_bloodParticlesBurstAmount);
             }
-            Debug.Log("Player being damaged");
+
+            if (_gameSceneManager != null)
+            {
+                PlayerInfo info = _gameSceneManager.GetPlayerInfo(col.GetInstanceID());
+                if (info != null && info.characterManager != null)
+                {
+                    info.characterManager.TakeDamage(_damageAmount);
+                }
+            }
         }
     }
 }
--- Dead Earth Game/Assets/Dead Earth/Scripts/FPS Controller/CharacterManager.cs
+++ Dead Earth Game/Assets/Dead Earth/Scripts/FPS Controller/CharacterManager.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Inspector Assigned
+    [SerializeField] private CapsuleCollider _meleeTrigger = null;
+    [SerializeField] private CameraBloodEffect _cameraBloodEffect = null;
+    [SerializeField] private Camera _camera = null;
+    [SerializeField] private float _health = 100.0f;
+
+    // Private
+    private Collider _collider = null;
+    private FPSController _fpsController = null;
+    private CharacterController _characterController = null;
+    private GameSceneManager _gameSceneManager = null;
+
+    // Use this for initialization
     void Start()
     {
-        
+        _collider = GetComponent<Collider>();
+        _fpsController = GetComponent<FPSController>();
+        _characterController = GetComponent<CharacterController>();
+        _gameSceneManager = GameSceneManager.instance;
+
+        if (_gameSceneManager != null)
+        {
+            PlayerInfo info = new PlayerInfo();
+            info.camera = _camera;
+            info.characterManager = this;
+            info.collider = _collider;
+            info.meleeTrigger = _meleeTrigger;
+
+            _gameSceneManager.RegisterPlayerInfo(_collider.GetInstanceID(), info);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float amount)
     {
-        
+        _health = Mathf.Max(_health - (amount * Time.deltaTime), 0.0f);
+        if (_cameraBloodEffect != null)
+        {
+            _cameraBloodEffect.minBloodAmount = (1.0f - _health / 100.0f);
+            _cameraBloodEffect.bloodAmount = Mathf.Min(_cameraBloodEffect.minBloodAmount + 0.3f, 1.0f);
+        }
     }
 }
